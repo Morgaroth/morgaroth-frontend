8805eb6ff90d7435bf8b813aa36d820ec92055df
Morgaroth <mateuszjaje@gmail.com>
2017-04-26 18:04:46 +0200

    refactored

--- app/containers/views/EventsLog.js
+++ app/containers/views/EventsLog.js
@@ -2,17 +2,6 @@ import React, {Component, PropTypes} from "react";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import * as Actions from "../../actions";
-import TeamInfoPage from "./TeamInfoPage";
-import DateInfoPage from "./DateInfoPage";
-import CouponInfoPage from "./CouponInfoPage";
-import BetInfoPage from "./GPBettingLeague";
-import Shortcuts from "../additional/Shortcuts";
-import DataBaseMethods from "../additional/DataBaseMethods";
-import VersionsMatrix from "../additional/VersionsMatrix";
-import BetBrowser from "../additional/BetBrowser";
-import LivePromptPage from "../additional/LivePromptPage";
-import ServerHealth from "../additional/ServerHealth";
-import DateRangeInfoPage from "./DateRangeInfoPage";
 
 class EventsLog extends Component {
 
@@ -32,15 +21,11 @@ EventsLog.propTypes = {
   actions: PropTypes.object.isRequired,
   cls: PropTypes.string,
   logs: PropTypes.array,
-  // status: PropTypes.string,
-  // data: PropTypes.object,
 };
 
 function mapStateToProps(state) {
   return {
     logs: state.eventsLog.logs,
-    // data: state.infoPage.data,
-    // status: state.infoPage.status || null,
   };
 }
 
